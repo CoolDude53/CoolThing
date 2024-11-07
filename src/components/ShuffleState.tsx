4850077d4377161492b298d9228993fc97dd3a2e
Jake Schafer <pickle.plaza@gmail.com>
2024-11-07 14:16:55 -0600

    fix repeat track icon

--- src/components/ShuffleState.tsx
+++ src/components/ShuffleState.tsx
@@ -122,9 +122,9 @@ const ShuffleState: React.FC = () => {
                 fill="currentColor"
                 className="h-12 w-12 text-brand"
               >
-                <path d="M0 4.75A3.75 3.75 0 0 1 3.75 1h8.5A3.75 3.75 0 0 1 16 4.75v5a3.75 3.75 0 0 1-3.75 3.75H9.81l1.018 1.018a.75.75 0 1 1-1.06 1.06L6.939 12.75l2.829-2.828a.75.75 0 1 1 1.06 1.06L9.811 12h2.439a2.25 2.25 0 0 0 2.25-2.25v-5a2.25 2.25 0 0 0-2.25-2.25h-12.5A12.25 2.25 0 0 0 1.5 4.75v5A2.25 2.25 0 0 0 3.75 12H5v1.5H3.75A3.75 3.75 0 0 1 0 9.75v-5z" />
+                <path d="M0 4.75A3.75 3.75 0 0 1 3.75 1h8.5A3.75 3.75 0 0 1 16 4.75v5a3.75 3.75 0 0 1-3.75 3.75H9.81l1.018 1.018a.75.75 0 1 1-1.06 1.06L6.939 12.75l2.829-2.828a.75.75 0 1 1 1.06 1.06L9.811 12h2.439a2.25 2.25 0 0 0 2.25-2.25v-5a2.25 2.25 0 0 0-2.25-2.25h-8.5A2.25 2.25 0 0 0 1.5 4.75v5A2.25 2.25 0 0 0 3.75 12H5v1.5H3.75A3.75 3.75 0 0 1 0 9.75v-5z" />
               </svg>
-              <div className="absolute left-1/2 top-2 -translate-x-1/2 -translate-y-1 transform text-xs font-bold text-brand">
+              <div className="absolute left-1/2 top-3.5 -translate-x-1/2 -translate-y-1 transform text-sm font-bold text-brand">
                 1
               </div>
             </div>
